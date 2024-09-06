bee2cf35ce555301dd38d6d8ca782d9941180611
edpelaezc <eapelaezc@correo.url.edu.g>
2024-09-05 20:19:16 -0600

    chore: project setup

---
+++ account-command/src/main/java/com/techbank/accountcommand/AccountCommandApplication.java
@@ -0,0 +1,13 @@
+package com.techbank.accountcommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCommandApplication.class, args);
+	}
+
+}
